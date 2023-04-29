a1bffb778171caee7512c2cc0a46c813df27f65c
Juan Esteban Sierra Agudelo <118354526+JuanEsteban546@users.noreply.github.com>
2023-04-29 17:45:33 -0500

    Update promociones.js

--- Js/promociones.js
+++ Js/promociones.js
@@ -27,6 +27,11 @@ let technology = document.querySelector('.technology');
 let homeAppliances = document.querySelector('.homeAppliances')
 let toys = document.querySelector('.toys');
 
+let clothingHambur = document.querySelector('.clothingHambur')
+let technologyHambur = document.querySelector('.technologyHambur')
+let homeAppliancesHambur = document.querySelector('.homeAppliancesHambur')
+let toysHambur = document.querySelector('.toysHambur')
+
 let promotionsList = [];
 let emptyCar = [];
 let informacionProd = [];
@@ -716,6 +721,31 @@ if(toys){
     })
 }
 
+// Navegacion ambur
+
+if(clothingHambur){
+    clothingHambur.addEventListener('click', ()=>{
+        location.href = 'ropa.html'
+    })
+}
+
+if(technologyHambur){
+    technologyHambur.addEventListener('click', ()=>{
+        location.href = 'tecnologia.html'
+    })
+}
+
+if(homeAppliancesHambur){
+    homeAppliancesHambur.addEventListener('click', ()=>{
+        location.href = 'electroDomesticos.html'
+    })
+}
+
+if(toysHambur){
+    toysHambur.addEventListener('click', ()=>{
+        location.href = 'juguetes.html'
+    })
+}
 
 if(containerSell){
     containerSell.addEventListener('click', ()=>{
